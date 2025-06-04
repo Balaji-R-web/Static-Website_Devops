112f195e977cbd3246b9fdf44814104ea342b20f
Balaji-R-web <balajir182001@gmail.com>
2025-06-04 17:47:17 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "site_policy" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "bucket_name" {
+  default = "your-unique-static-site-bucket-name"
+}
